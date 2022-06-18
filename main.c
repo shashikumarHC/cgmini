8684bb21ed0af59de2452d611aa3ffd71015b730
shashikumaracharya <73772440+shashikumarHC@users.noreply.github.com>
2022-06-19 00:37:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
  #include <GL/glut.h>
 #include <stdio.h>
 #include<math.h>
-int b=20,c=600,d=700,f=650,g=1300,h=700;
+int b=20,c=600,d=700,f=650,g=1300,h=700,l=100,m=500;
 void myInit()
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -11,18 +11,42 @@ void myInit()
 }
 void myDisplayday()
 {
-    void tri(int v,int w,int x,int y,int z) //tree
+    void tree(int x,int y) //tree
     {
-        glBegin(GL_TRIANGLES);
-        glColor3f(0.0,1.0,0.0);
-        glVertex2f(v,w);
-        glVertex2f(x,y);
-        glVertex2f(z,w);
-        glEnd();
+        float a;
+        glBegin(GL_POLYGON);
+        glColor3f(0,1.0,0);
+        for (int i=0;i<360;i++)
+        {
+            a=i*3.142/180;
+            glVertex2f(x+60*cos(a),y+60*sin(a));
+        }
+        for (int i=0;i<360;i++)
+        {
+            a=i*3.142/180;
+            glVertex2f(x+50+60*cos(a),y+70+60*sin(a));
+        }
+        for (int i=0;i<360;i++)
+        {
+            a=i*3.142/180;
+            glVertex2f(x+120+60*cos(a),y+70+60*sin(a));
+        }
+        for (int i=0;i<360;i++)
+        {
+            a=i*3.142/180;
+            glVertex2f(x+160+60*cos(a),y+60*sin(a));
+        }
+        for (int i=0;i<360;i++)
+        {
+            a=i*3.142/180;
+            glVertex2f(x+85+60*cos(a),y-10+60*sin(a));
+        }
+    glEnd();
+
     }
     glClear(GL_COLOR_BUFFER_BIT);
-        glBegin(GL_POLYGON);
-        glColor3f(0.678, 0.847, 0.902);
+    glBegin(GL_POLYGON);
+        glColor3f(0.67,0.84,0.90);
         glVertex2i(0,0);
         glVertex2i(0,800);
         glVertex2i(1800,800);
@@ -30,38 +54,104 @@ void myDisplayday()
     glEnd();
     float a;
     glBegin(GL_POLYGON); //sun
-        glColor3f(1.0,1.0,0.0);
+        glColor3f(1.0,1.0,0);
         for (int i=0;i<360;i++)
         {
             a=i*3.142/180;
             glVertex2f(900+80*cos(a),700+80*sin(a));
         }
     glEnd();
-    glBegin(GL_POLYGON); // sand
-        glColor3f(0.70,0.39,0.11);
+    glBegin(GL_POLYGON); //sand
+        glColor3f(0.92,0.64,0.51);
         glVertex2f(0,0);
-        glVertex2f(0,150);
-        glVertex2f(900,300);
-        glVertex2f(1800,150);
+        glVertex2f(0,300);
+        glVertex2f(900,400);
+        glVertex2f(1800,300);
         glVertex2f(1800,0);
     glEnd();
-    glBegin(GL_POLYGON);  //road
-        glColor3f(0.0,0.0,0.0);
-        glVertex2f(700,0);
-        glVertex2f(900,300);
-        glVertex2f(1100,0);
-    glEnd();
-    glBegin(GL_POLYGON); // roadline
-        glColor3f(1.0,1.0,1.0);
-        glVertex2f(895,0);
-        glVertex2f(900,300);
-        glVertex2f(905,0);
-    glEnd();
-    tri(100,20,180,200,280);
-    tri(70,60,180,200,310);
-    tri(1520,20,1620,200,1700);
-    tri(1490,60,1620,200,1730);
-    glBegin(GL_POLYGON); //cloud1
+
+    glBegin(GL_POLYGON); //tree1
+      glColor3f(0.44,0.33,0.23);
+        glVertex2i(135,150);
+
+        glVertex2i(235,150);
+
+        glVertex2i(235,300);
+        glVertex2i(135,300);
+        glEnd();
+
+        glBegin(GL_LINE_LOOP); //tree1 border
+      glColor3f(0,0,0);
+        glVertex2i(135,150);
+
+        glVertex2i(235,150);
+
+        glVertex2i(235,300);
+        glVertex2i(135,300);
+        glEnd();
+
+        glBegin(GL_POLYGON); //tree2
+        glColor3f(0.44,0.33,0.23);
+
+        glVertex2i(1535,150);
+        glVertex2i(1635,150);
+        glVertex2i(1635,300);
+        glVertex2i(1535,300);
+        glEnd();
+
+        glBegin(GL_LINE_LOOP); //tree2 border
+      glColor3f(0,0,0);
+        glVertex2i(1535,150);
+        glVertex2i(1635,150);
+        glVertex2i(1635,300);
+        glVertex2i(1535,400);
+        glEnd();
+
+        glBegin(GL_POLYGON); //tree3
+      glColor3f(0.44,0.33,0.23);
+        glVertex2i(385,250);
+
+        glVertex2i(485,250);
+
+        glVertex2i(485,400);
+        glVertex2i(385,400);
+        glEnd();
+
+        glBegin(GL_LINE_LOOP); //tree3 border
+      glColor3f(0,0,0);
+        glVertex2i(385,250);
+
+        glVertex2i(485,250);
+
+        glVertex2i(485,400);
+        glVertex2i(385,400);
+        glEnd();
+
+        glBegin(GL_POLYGON); //tree4
+      glColor3f(0.44,0.33,0.23);
+        glVertex2i(1285,250);
+
+        glVertex2i(1385,250);
+
+        glVertex2i(1385,400);
+        glVertex2i(1285,400);
+        glEnd();
+
+        glBegin(GL_LINE_LOOP); //tree4 border
+      glColor3f(0,0,0);
+        glVertex2i(1285,250);
+
+        glVertex2i(1385,250);
+
+        glVertex2i(1385,400);
+        glVertex2i(1285,400);
+        glEnd();
+
+    tree(100,300);
+    tree(350,400);
+    tree(1250,400);
+    tree(1500,300);
+    glBegin(GL_POLYGON); //cloud 1
         glColor3f(1.0,1.0,1.0);
         for (int i=0;i<360;i++)
         {
@@ -90,7 +180,7 @@ void myDisplayday()
         }
     glEnd();
 
-    glBegin(GL_POLYGON); //cloud2
+    glBegin(GL_POLYGON); //cloud 2
         glColor3f(1.0,1.0,1.0);
         for (int i=0;i<360;i++)
         {
@@ -149,77 +239,202 @@ void myDisplayday()
         }
     glEnd();
 
-    glBegin(GL_POLYGON); //house
-        glColor3f(0.99,0.87,0.69);
-        glVertex2i(600,100);
-        glVertex2i(600,450);
-        glVertex2i(1200,450);
-        glVertex2i(1200,100);
-    glEnd();
+    glColor3f(0.92,0.64,0.51);
+glBegin(GL_POLYGON); //roof back
+glVertex2d(700,500);
+glVertex2d(1150,500);
+glVertex2d(1250,400);
+glVertex2d(800,400);
+glEnd();
+glColor3f(0,0,0);
+glBegin(GL_LINE_LOOP); //roof back border
+glVertex2d(700,500);
+glVertex2d(1150,500);
+glVertex2d(1250,400);
+glVertex2d(800,400);
+glEnd();
+glColor3f(0.99,0.87,0.69); //main house
+glBegin(GL_POLYGON);
+glVertex2d(600,200);
+glVertex2d(1000,200);
+glVertex2d(1000,400);
+glVertex2d(600,400);
+glEnd();
+glColor3f(0,0,0);
+glBegin(GL_LINE_LOOP); //main house border
+glVertex2d(600,200);
+glVertex2d(1000,200);
+glVertex2d(1000,400);
+glVertex2d(600,400);
+glEnd();
+glColor3f(0.99,0.87,0.69); //side home
+glBegin(GL_POLYGON);
+glVertex2d(1000,200);
+glVertex2d(1000,400);
+glVertex2d(1100,500);
+glVertex2d(1200,400);
+glVertex2d(1200,250);
+glEnd();
 
-    glBegin(GL_POLYGON); //roof
-        glColor3f(0.92,0.64,0.51);
-        glVertex2i(500,400);
-        glVertex2i(700,550);
-        glVertex2i(1100,550);
-        glVertex2i(1300,400);
-    glEnd();
+glColor3f(0,0,0);
+glBegin(GL_LINE_LOOP); //side home border
+glVertex2d(1000,200);
+glVertex2d(1000,400);
+glVertex2d(1100,500);
+glVertex2d(1200,400);
+glVertex2d(1200,250);
+glEnd();
 
-    glBegin(GL_POLYGON); //doar
-        glColor3f(1.0,1.0,1.0);
-        glVertex2i(825,100);
-        glVertex2i(825,300);
-        glVertex2i(975,300);
-        glVertex2i(975,100);
-    glEnd();
-
+glColor3f(0.92,0.64,0.51); //roof front
+glBegin(GL_POLYGON);
+glVertex2d(700,500);
+glVertex2d(1150,500);
+glVertex2d(1000,400);
+glVertex2d(550,400);
+glEnd();
+glColor3f(0,0,0);
+glBegin(GL_LINE_LOOP); //roof front border
+glVertex2d(700,500);
+glVertex2d(1150,500);
+glVertex2d(1000,400);
+glVertex2d(550,400);
+glEnd();
     glBegin(GL_POLYGON); //window1
         glColor3f(1.0,1.0,1.0);
-        glVertex2i(650,250);
-        glVertex2i(650,350);
-        glVertex2i(750,350);
-        glVertex2i(750,250);
+        glVertex2i(625,275);
+        glVertex2i(625,350);
+        glVertex2i(700,350);
+        glVertex2i(700,275);
+    glEnd();
+    glBegin(GL_LINE_LOOP); //window1 outline
+        glColor3f(0,0,0);
+        glVertex2i(625,275);
+        glVertex2i(625,350);
+        glVertex2i(700,350);
+        glVertex2i(700,275);
+    glEnd();
+
+    glBegin(GL_POLYGON); //window3
+        glColor3f(1.0,1.0,1.0);
+        glVertex2i(975,275);
+        glVertex2i(975,350);
+        glVertex2i(900,350);
+        glVertex2i(900,275);
+    glEnd();
+
+    glBegin(GL_LINE_LOOP); //window3 outline
+        glColor3f(0,0,0);
+        glVertex2i(975,275);
+        glVertex2i(975,350);
+        glVertex2i(900,350);
+        glVertex2i(900,275);
     glEnd();
 
     glBegin(GL_POLYGON); //window2
         glColor3f(1.0,1.0,1.0);
-        glVertex2i(1150,250);
-        glVertex2i(1150,350);
+        glVertex2i(1150,270);
+        glVertex2i(1150,370);
         glVertex2i(1050,350);
         glVertex2i(1050,250);
     glEnd();
 
-    glBegin(GL_POLYGON); //solarpanal
+    glBegin(GL_LINE_LOOP); //window2 outline
         glColor3f(0,0,0);
-        glVertex2i(700,425);
-        glVertex2i(700,525);
-        glVertex2i(1100,525);
-        glVertex2i(1100,425);
+        glVertex2i(1150,270);
+        glVertex2i(1150,370);
+        glVertex2i(1050,350);
+        glVertex2i(1050,250);
+    glEnd();
+    glBegin(GL_POLYGON); //door
+        glColor3f(1.0,1.0,1.0);
+        glVertex2i(750,200);
+        glVertex2i(750,300);
+        glVertex2i(850,300);
+        glVertex2i(850,200);
+    glEnd();
+
+    glBegin(GL_LINE_LOOP); //door outline
+        glColor3f(0,0,0);
+        glVertex2i(750,200);
+        glVertex2i(750,300);
+        glVertex2i(850,300);
+        glVertex2i(850,200);
+    glEnd();
+
+    glBegin(GL_POLYGON); //solar panel
+        glColor3f(0,0,0);
+        glVertex2i(720,475);
+        glVertex2i(1040,475);
+        glVertex2i(970,425);
+        glVertex2i(650,425);
     glEnd();
 
     glBegin(GL_LINES);
         glColor3f(1.0,1.0,1.0); //line for solar panel
-        for(int i=700;i<=1100;i=i+25)
+        for(int i=720;i<=1040;i=i+32)
         {
-            glVertex2i(i,425);
-            glVertex2i(i,525);
+            glVertex2i(i-70,425);
+            glVertex2i(i,475);
         }
-        for(int i=425;i<=525;i=i+25)
+        int i,j;
+        for(i=425,j=650;i<=475;i=i+(50/2),j=j+35)
         {
-            glVertex2i(700,i);
-            glVertex2i(1100,i);
+            glVertex2i(j,i);
+            glVertex2i(j+320,i);
         }
-        glColor3f(0,0,0);  //line for window
-        glVertex2i(650,300);
-        glVertex2i(750,300);
-        glVertex2i(700,250);
-        glVertex2i(700,350);
+        glColor3f(0,0,0);
+        glVertex2f(625,307.5); //line for windows
+        glVertex2i(700,307.5);
+        glVertex2i(662.5,275);
+        glVertex2i(662.5,350);
+
+        glVertex2f(900,307.5);
+        glVertex2i(975,307.5);
+        glVertex2i(937.5,275);
+        glVertex2i(937.5,350);
 
         glVertex2i(1050,300);
-        glVertex2i(1150,300);
-        glVertex2i(1100,250);
-        glVertex2i(1100,350);
+        glVertex2i(1150,320);
+        glVertex2i(1100,260);
+        glVertex2i(1100,360);
 
+    glEnd();
+
+    glColor3f(0,0,0); //road
+    glBegin(GL_POLYGON);
+    glVertex2i(0,0);
+    glVertex2i(0,100);
+    glVertex2i(1800,100);
+    glVertex2i(1800,0);
+    glEnd();
+    glColor3f(1,1,1);
+    glBegin(GL_LINES);
+    glVertex2i(0,50);
+
+    glVertex2i(1800,50);
+
+    glEnd();
+
+    //car
+    glColor3f(1,0,0);
+    glBegin(GL_POLYGON);
+    glVertex2i(m,55);
+    glVertex2i(m,75);
+    glVertex2i(m+70,75);
+    glVertex2i(m+70,55);
+    glEnd();
+    glColor3f(1,0,0);
+    glBegin(GL_POLYGON);
+    glVertex2i(m+10,75);
+    glVertex2i(m+20,95);
+    glVertex2i(m+50,95);
+    glVertex2i(m+60,75);
+    glEnd();
+    glColor3f(1,1,1);
+    glPointSize(10.0);
+    glBegin(GL_POINTS);
+    glVertex2i(m+15,55);
+    glVertex2i(m+55,55);
     glEnd();
 
     glFlush();
@@ -233,7 +448,7 @@ void myDisplaynight()
     {
         float a;
         glBegin(GL_POLYGON);
-        glColor3f(0,1.0,0);
+        glColor3f(0,0.6,0);
         for (int i=0;i<360;i++)
         {
             a=i*3.142/180;
@@ -282,19 +497,95 @@ void myDisplaynight()
     glBegin(GL_POLYGON); //sand
         glColor3f(0.70,0.39,0.11);
         glVertex2f(0,0);
-        glVertex2f(0,150);
-        glVertex2f(900,300);
-        glVertex2f(1800,150);
+        glVertex2f(0,300);
+        glVertex2f(900,400);
+        glVertex2f(1800,300);
         glVertex2f(1800,0);
     glEnd();
 
 
+    glBegin(GL_POLYGON); //tree1
+      glColor3f(0.44,0.33,0.23);
+        glVertex2i(135,150);
 
+        glVertex2i(235,150);
 
-    tree(100,200);
-    tree(900,1000);
- /*   tri(1520,20,1620,200,1700);
-    tri(1490,60,1620,200,1730); */
+        glVertex2i(235,300);
+        glVertex2i(135,300);
+        glEnd();
+
+        glBegin(GL_LINE_LOOP); //tree1 border
+      glColor3f(0,0,0);
+        glVertex2i(135,150);
+
+        glVertex2i(235,150);
+
+        glVertex2i(235,300);
+        glVertex2i(135,300);
+        glEnd();
+
+        glBegin(GL_POLYGON); //tree2
+        glColor3f(0.44,0.33,0.23);
+
+        glVertex2i(1535,150);
+        glVertex2i(1635,150);
+        glVertex2i(1635,300);
+        glVertex2i(1535,300);
+        glEnd();
+
+        glBegin(GL_LINE_LOOP); //tree2 border
+      glColor3f(0,0,0);
+        glVertex2i(1535,150);
+        glVertex2i(1635,150);
+        glVertex2i(1635,300);
+        glVertex2i(1535,400);
+        glEnd();
+
+        glBegin(GL_POLYGON); //tree3
+      glColor3f(0.44,0.33,0.23);
+        glVertex2i(385,250);
+
+        glVertex2i(485,250);
+
+        glVertex2i(485,400);
+        glVertex2i(385,400);
+        glEnd();
+
+        glBegin(GL_LINE_LOOP); //tree3 border
+      glColor3f(0,0,0);
+        glVertex2i(385,250);
+
+        glVertex2i(485,250);
+
+        glVertex2i(485,400);
+        glVertex2i(385,400);
+        glEnd();
+
+        glBegin(GL_POLYGON); //tree4
+      glColor3f(0.44,0.33,0.23);
+        glVertex2i(1285,250);
+
+        glVertex2i(1385,250);
+
+        glVertex2i(1385,400);
+        glVertex2i(1285,400);
+        glEnd();
+
+        glBegin(GL_LINE_LOOP); //tree4 border
+      glColor3f(0,0,0);
+        glVertex2i(1285,250);
+
+        glVertex2i(1385,250);
+
+        glVertex2i(1385,400);
+        glVertex2i(1285,400);
+        glEnd();
+
+    tree(100,300);
+    tree(350,400);
+    tree(1250,400);
+    tree(1500,300);
+
     glBegin(GL_POLYGON); //cloud 1
         glColor3f(1.0,1.0,1.0);
         for (int i=0;i<360;i++)
@@ -384,34 +675,34 @@ void myDisplaynight()
     glEnd();
 
     glColor3f(0.70,0.39,0.11);
-glBegin(GL_POLYGON);
+glBegin(GL_POLYGON); //roof back
 glVertex2d(700,500);
 glVertex2d(1150,500);
 glVertex2d(1250,400);
 glVertex2d(800,400);
 glEnd();
 glColor3f(0,0,0);
-glBegin(GL_LINE_LOOP);
+glBegin(GL_LINE_LOOP); //roof back border
 glVertex2d(700,500);
 glVertex2d(1150,500);
 glVertex2d(1250,400);
 glVertex2d(800,400);
 glEnd();
-glColor3f(0.99,0.87,0.69);
-glBegin(GL_POLYGON);
+glColor3f(0.99,0.77,0.69);
+glBegin(GL_POLYGON); //main house
 glVertex2d(600,200);
 glVertex2d(1000,200);
 glVertex2d(1000,400);
 glVertex2d(600,400);
 glEnd();
 glColor3f(0,0,0);
-glBegin(GL_LINE_LOOP);
+glBegin(GL_LINE_LOOP); //main house border
 glVertex2d(600,200);
 glVertex2d(1000,200);
 glVertex2d(1000,400);
 glVertex2d(600,400);
 glEnd();
-glColor3f(0.99,0.87,0.69); //side home
+glColor3f(0.99,0.77,0.69); //side home
 glBegin(GL_POLYGON);
 glVertex2d(1000,200);
 glVertex2d(1000,400);
@@ -419,15 +710,25 @@ glVertex2d(1100,500);
 glVertex2d(1200,400);
 glVertex2d(1200,250);
 glEnd();
+
+glColor3f(0,0,0);
+glBegin(GL_LINE_LOOP); //side home border
+glVertex2d(1000,200);
+glVertex2d(1000,400);
+glVertex2d(1100,500);
+glVertex2d(1200,400);
+glVertex2d(1200,250);
+glEnd();
+
 glColor3f(0.70,0.39,0.11);
-glBegin(GL_POLYGON);
+glBegin(GL_POLYGON);  //roof front
 glVertex2d(700,500);
 glVertex2d(1150,500);
 glVertex2d(1000,400);
 glVertex2d(550,400);
 glEnd();
 glColor3f(0,0,0);
-glBegin(GL_LINE_LOOP);
+glBegin(GL_LINE_LOOP); //roof front border
 glVertex2d(700,500);
 glVertex2d(1150,500);
 glVertex2d(1000,400);
@@ -435,6 +736,14 @@ glVertex2d(550,400);
 glEnd();
     glBegin(GL_POLYGON); //window1
         glColor3f(1.0,1.0,0.0);
+        glVertex2i(625,275);
+        glVertex2i(625,350);
+        glVertex2i(700,350);
+        glVertex2i(700,275);
+    glEnd();
+
+    glBegin(GL_LINE_LOOP); //window1 outline
+        glColor3f(0,0,0);
         glVertex2i(625,275);
         glVertex2i(625,350);
         glVertex2i(700,350);
@@ -449,6 +758,14 @@ glEnd();
         glVertex2i(900,275);
     glEnd();
 
+    glBegin(GL_LINE_LOOP); //window3 outline
+        glColor3f(0,0,0);
+        glVertex2i(975,275);
+        glVertex2i(975,350);
+        glVertex2i(900,350);
+        glVertex2i(900,275);
+    glEnd();
+
     glBegin(GL_POLYGON); //window2
         glColor3f(1.0,1.0,0.0);
         glVertex2i(1150,270);
@@ -456,8 +773,25 @@ glEnd();
         glVertex2i(1050,350);
         glVertex2i(1050,250);
     glEnd();
+
+    glBegin(GL_LINE_LOOP); //window2 outline
+        glColor3f(0,0,0);
+        glVertex2i(1150,270);
+        glVertex2i(1150,370);
+        glVertex2i(1050,350);
+        glVertex2i(1050,250);
+    glEnd();
+
     glBegin(GL_POLYGON); //door
         glColor3f(1.0,1.0,0.0);
+        glVertex2i(750,200);
+        glVertex2i(750,300);
+        glVertex2i(850,300);
+        glVertex2i(850,200);
+    glEnd();
+
+    glBegin(GL_LINE_LOOP); //door outline
+        glColor3f(0,0,0);
         glVertex2i(750,200);
         glVertex2i(750,300);
         glVertex2i(850,300);
@@ -503,6 +837,43 @@ glEnd();
 
     glEnd();
 
+    glColor3f(0,0,0);
+    glBegin(GL_POLYGON); //road
+    glVertex2i(0,0);
+    glVertex2i(0,100);
+    glVertex2i(1800,100);
+    glVertex2i(1800,0);
+    glEnd();
+    glColor3f(1,1,1);
+    glBegin(GL_LINES);
+    glVertex2i(0,50);
+
+    glVertex2i(1800,50);
+
+    glEnd();
+
+    //car
+    glColor3f(1,1,0);
+    glBegin(GL_POLYGON);
+    glVertex2i(l,55);
+    glVertex2i(l,75);
+    glVertex2i(l+70,75);
+    glVertex2i(l+70,55);
+    glEnd();
+    glColor3f(1,1,0);
+    glBegin(GL_POLYGON);
+    glVertex2i(l+10,75);
+    glVertex2i(l+20,95);
+    glVertex2i(l+50,95);
+    glVertex2i(l+60,75);
+    glEnd();
+    glColor3f(1,1,1);
+    glPointSize(10.0);
+    glBegin(GL_POINTS);
+    glVertex2i(l+15,55);
+    glVertex2i(l+55,55);
+    glEnd();
+
     glFlush();
     glutSwapBuffers();
 }
@@ -525,6 +896,15 @@ void re(int)
         g+=10;
     else
         g=0;
+    if (l<1850)
+        l+=5;
+    else
+        l=0;
+    if (m>-50)
+        m-=5;
+    else
+        m=1800;
+
 
 }
 
